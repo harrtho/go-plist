9dfcce80677705273f9b3d54861201b38a55bfec
zdypro <zdypro@zdypros-MacBook-Pro.local>
2020-05-05 17:25:44 +0400

    update map keyed

--- unmarshal.go
+++ unmarshal.go
@@ -248,7 +248,7 @@ func (p *Decoder) unmarshalDictionary(dict *cfDictionary, val reflect.Value) {
 	typ := val.Type()
 	switch val.Kind() {
 	case reflect.Struct:
-		tinfo, err := getTypeInfo(typ)
+		tinfo, err := GetTypeInfo(typ)
 		if err != nil {
 			panic(err)
 		}
@@ -259,8 +259,8 @@ func (p *Decoder) unmarshalDictionary(dict *cfDictionary, val reflect.Value) {
 			entries[k] = sval
 		}
 
-		for _, finfo := range tinfo.fields {
-			p.unmarshal(entries[finfo.name], finfo.value(val))
+		for _, finfo := range tinfo.Fields {
+			p.unmarshal(entries[finfo.Name], finfo.Value(val))
 		}
 	case reflect.Map:
 		if val.IsNil() {
